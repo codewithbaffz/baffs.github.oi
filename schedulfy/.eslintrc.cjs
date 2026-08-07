512d82afc1af2bb9460fd9183285a4ee2930b358
codewithbaffz <kojobaffs@gmail.com>
2026-08-07 16:56:18 +0000

    chore(lint): revert unused-imports plugin rules (option A)

--- schedulfy/.eslintrc.cjs
+++ schedulfy/.eslintrc.cjs
@@ -19,10 +19,5 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'no-unused-vars': 'off',
     'react-refresh/only-export-components': 'off',
-    // Turn off strict unused-imports rules to reduce noise in CI. We'll prefer
-    // fixing actual runtime issues (like undefined variables) rather than failing
-    // the build for icon imports that are intentionally left for future use.
-    'unused-imports/no-unused-imports': 'off',
-    'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
   },
 };
